d90fab2ce4771edb2b179ab6c0ed1f449b7ea1cf
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-12-04 16:32:30 +0100

    added: Get, Post, Delete category

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,45 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
-@Controller()
+type TCategory = {
+  id: number;
+  name: string;
+};
+@Controller('categories')
 export class AppController {
+  private categories: TCategory[] = [
+    { id: 1, name: 'Groceries' },
+    { id: 2, name: 'Cosmetics' },
+    { id: 3, name: 'Toys' },
+    { id: 4, name: 'Dairy' },
+    { id: 5, name: 'Fashion' },
+    { id: 6, name: 'Electronics' },
+    { id: 7, name: 'Games' },
+  ];
+
   @Get()
-  getHello(): string {
-    return 'Hello World';
+  getAllCategories() {
+    return this.categories;
   }
 
-  @Get('cat/:catId')
-  getMyCat(): { name: string; id: number } {
-    return { name: 'Killer', id: 100 };
+  @Get(':id')
+  getSingleCategory(@Param('id') categoryId: string) {
+    return this.categories.find(({ id }) => id === Number(categoryId));
+  }
+
+  @Post()
+  addNewCategory(@Body() payload: { name: string }) {
+    const category: TCategory = { id: this.categories.length + 1, ...payload };
+    this.categories.push(category);
+    return category;
+  }
+
+  @Delete(':name')
+  deleteCategory(@Param('name') categoryName: string): { message: string } {
+    this.categories = this.categories.filter(
+      ({ name }) => name !== categoryName,
+    );
+    return {
+      message: `${categoryName} deleted`,
+    };
   }
 }
