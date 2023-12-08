c52829094a7307b2c206b8a214a8b577e3f043cf
Sebastian Komorowski <sebastian.komorowski1@outlook.com>
2023-12-08 20:43:45 +0100

    used PartialType to create updateProduct DTO

--- src/products/dto/update-product.dto.ts
+++ src/products/dto/update-product.dto.ts
@@ -1,43 +1,4 @@
-// export interface Product {
-//     id: number;
-//     name: string;
-//     price: number;
-//     stock: number;
-//     image: string;
-//     description?: string;
-//   }
+import { PartialType } from '@nestjs/mapped-types';
+import { NewProductDto } from './new-product.dto';
 
-import {
-  IsInt,
-  IsNotEmpty,
-  IsNumber,
-  IsOptional,
-  IsPositive,
-  IsString,
-  IsUrl,
-} from 'class-validator';
-
-export class UpdateProductDto {
-  @IsString()
-  @IsNotEmpty()
-  @IsOptional()
-  name?: string;
-
-  @IsNumber({ maxDecimalPlaces: 2 })
-  @IsOptional()
-  price?: number;
-
-  @IsString()
-  @IsOptional()
-  description?: string;
-
-  @IsUrl({ require_protocol: true })
-  @IsOptional()
-  image?: string;
-
-  @IsNumber()
-  @IsInt()
-  @IsOptional()
-  @IsPositive()
-  stock?: number;
-}
+export class UpdateProductDto extends PartialType(NewProductDto) {}
